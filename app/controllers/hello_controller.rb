350b4882e413ca16ce2e05fac90f3be09b739dad
yojir <yo0102@icloud.com>
2022-05-12 11:37:28 +0900

    for文

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,6 +2,7 @@ class HelloController < ApplicationController
   def index
     @hello = "Hello World"
     @food = "aburaSoba"
+    @foods = ["a", "b", "c", "d", "e"]
     render template: "hello/index"
   end  
 end
